beb7032d24cb49eb1586aa1c472cf308161d6423
Almaz Bisenbaev <almaz9527@gmail.com>
2025-08-06 21:21:27 +0500

    Fixed layout issues in fladhcard component

--- src/components/Flashcard.tsx
+++ src/components/Flashcard.tsx
@@ -12,7 +12,7 @@ const Flashcard: React.FC<FlashcardProps> = ({ letter, onNext, onLearned }) => {
           <div className="flashcard-front">
             <div className='flashcard-character'>{letter.character}</div>
             <button 
-              className="btn btn-primary"
+              className="btn btn-primary w-full"
               onClick={() => setShowAnswer(true)}
             >
               Show answer
@@ -20,34 +20,36 @@ const Flashcard: React.FC<FlashcardProps> = ({ letter, onNext, onLearned }) => {
           </div>
         ) : (
           <div className="flashcard-back">
-            <div className='flashcard-name'>{letter.name}</div>
-            <div className='flashcard-pronunciation'>
-              Pronunciation: <b>{letter.pronunciation}</b>
+            <div className='flashcard-back-content'>
+              <div className='flashcard-name'>{letter.name}</div>
+              <div className='flashcard-pronunciation'>
+                Pronunciation: <b>{letter.pronunciation}</b>
+              </div>
+              <div>
+                <audio
+                  controls
+                  className="flashcard-audio"
+                  src={letter.audioUrl}
+                  preload="auto"
+                >
+                  Your browser does not support the audio element.
+                </audio>
+              </div>
             </div>
-            <div>
-              <audio
-                controls
-                className="flashcard-audio"
-                src={letter.audioUrl}
-                preload="auto"
-              >
-                Your browser does not support the audio element.
-              </audio>
-            </div>
-            <div className="flashcard-actions">
-              <button 
-                className="btn btn-block btn-primary"
-                onClick={onNext}
-              >
-                Next card
-              </button>
-              <button 
-                className="btn btn-block"
-                onClick={onLearned}
-              >
-                Mark as learned
-              </button>
-            </div>
+			<div className="flashcard-actions">
+				<button 
+				className="btn btn-block btn-primary"
+				onClick={onNext}
+				>
+				Next card
+				</button>
+				<button 
+				className="btn btn-block"
+				onClick={onLearned}
+				>
+				Mark as learned
+				</button>
+			</div>
           </div>
         )}
       </div>
