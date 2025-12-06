9ba64e9ae9790e2f2d39014551e1fad8f76a6a7e
Almaz Bisenbaev <almaz9527@gmail.com>
2025-12-06 19:51:38 +0500

    Redesigned scope preview section

--- src/components/ScopePreview/scope-preview.tsx
+++ src/components/ScopePreview/scope-preview.tsx
@@ -4,45 +4,51 @@ import "./scope-preview.css";
 export default function ScopePreview() {
 
   return (
-    <div className="level-preview-section">
+    <div className="levels-preview-section">
 
-      <h3 className="level-preview-title">What you'll learn:</h3>
+      <h3 className="levels-preview-title">What you'll learn:</h3>
 
-      <div className="level-preview-grid">
+      <div className="levels-preview-list">
 
-        <div className="level-preview-item">
-          <div className="level-preview-icon">
+        <div className="levels-preview-item">
+          <div className="levels-preview-icon">
             <Image src="/images/icon-alphabet.svg" alt="Georgian Alphabet" width={38} height={38} />
           </div>
-          <div className="level-preview-content">
-            <div className="level-preview-name">Alphabet</div>
+          <div className="levels-preview-content">
+            <div className="levels-preview-name">Alphabet</div>
           </div>
         </div>
 
-        <div className="level-preview-item">
-          <div className="level-preview-icon">
+        <div className="levels-preview-divider"></div>
+
+        <div className="levels-preview-item">
+          <div className="levels-preview-icon">
             <Image src="/images/icon-numbers.svg" alt="Georgian Numbers" width={38} height={38} />
           </div>
-          <div className="level-preview-content">
-            <div className="level-preview-name">Numbers</div>
+          <div className="levels-preview-content">
+            <div className="levels-preview-name">Numbers</div>
           </div>
         </div>
 
-        <div className="level-preview-item">
-          <div className="level-preview-icon">
+        <div className="levels-preview-divider"></div>
+
+        <div className="levels-preview-item">
+          <div className="levels-preview-icon">
             <Image src="/images/icon-phrases.svg" alt="Georgian Words" width={38} height={38} />
           </div>
-          <div className="level-preview-content">
-            <div className="level-preview-name">Words</div>
+          <div className="levels-preview-content">
+            <div className="levels-preview-name">Words</div>
           </div>
         </div>
 
-        <div className="level-preview-item">
-          <div className="level-preview-icon">
+        <div className="levels-preview-divider"></div>
+
+        <div className="levels-preview-item">
+          <div className="levels-preview-icon">
             <Image src="/images/icon-phrases.svg" alt="Georgian Sentences" width={38} height={38} />
           </div>
-          <div className="level-preview-content">
-            <div className="level-preview-name">Sentences</div>
+          <div className="levels-preview-content">
+            <div className="levels-preview-name">Sentences</div>
           </div>
         </div>
 
