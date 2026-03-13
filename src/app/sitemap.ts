bcdce42a2c2f2ee590d252c1c09517de7265cd9f
Almaz Bisenbaev <almaz9527@gmail.com>
2026-03-13 10:12:12 +0500

    Add lessons to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,19 +9,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: 'https://tbilingo.vercel.app/learn/1',
+      url: 'https://tbilingo.vercel.app/lessons',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
     },
     {
-      url: 'https://tbilingo.vercel.app/learn/2',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://tbilingo.vercel.app/learn/3',
+      url: 'https://tbilingo.vercel.app/lessons/1',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.8,
